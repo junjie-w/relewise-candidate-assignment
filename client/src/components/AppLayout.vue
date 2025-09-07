fd98a1788894cf98de859ec2070d5272b1719118
junjie-w <junjiewu.brooke@gmail.com>
2025-09-07 21:29:42 +0200

    feat(client): update layout and root styling

--- client/src/components/AppLayout.vue
+++ client/src/components/AppLayout.vue
@@ -9,20 +9,21 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col">
-    <header class="flex justify-end items-center p-4">
-      <nav class="flex gap-4">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/">PDP</RouterLink>
-      </nav>
-    </header>
+  <div class="w-screen h-screen flex flex-col items-center">
+    <div class="w-4/5 h-full flex flex-col py-4 text-slate-500">
+      <header class="flex justify-end items-center">
+        <nav class="hover:underline cursor-pointer hover:italic transition-all duration-200 ease-out">
+          <RouterLink to="/">Home</RouterLink>
+        </nav>
+      </header>
 
-    <main class="flex-1">
-      <slot />
-    </main>
+      <main class="flex-1">
+        <slot />
+      </main>
 
-    <footer class="flex justify-center items-center p-4">
-      <p>Relewise Candidate Assignment</p>
-    </footer>
+      <footer class="flex justify-center items-center">
+        <a class="hover:underline cursor-pointer hover:italic transition-all duration-200 ease-out" href="https://product-pioneer-app.lovable.app/" target="_blank" rel="noopener noreferrer">Relewise Candidate Assignment</a>
+      </footer>
+    </div>
   </div>
 </template>
