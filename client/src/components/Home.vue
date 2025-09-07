48d76d9bb52f16bedf101996ee1764a4bbe3cb70
junjie-w <junjiewu.brooke@gmail.com>
2025-09-07 16:03:23 +0200

    feat(client): remove boilerplate and implement user id management

---
+++ client/src/components/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="p-8">
+    <h1 class="text-center text-gray-600">Home page</h1>
+  </div>
+</template>
---
+++ client/src/utils/userUtils.ts
@@ -0,0 +1,13 @@
+export const USER_ID_KEY = 'userId';
+
+export const getUserId = (): string => {
+  const existingUserId = localStorage.getItem(USER_ID_KEY);
+  
+  if (existingUserId) {
+    return existingUserId;
+  }
+  
+  const newUserId = crypto.randomUUID();
+  localStorage.setItem(USER_ID_KEY, newUserId);
+  return newUserId;
+};
