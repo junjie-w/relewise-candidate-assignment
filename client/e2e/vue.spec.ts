979061729d30eb0b49a6cf940c2ddf7f3c585f06
junjie-w <junjiewu.brooke@gmail.com>
2025-09-09 08:49:54 +0200

    fix(e2e): fix footer e2e test

--- client/e2e/vue.spec.ts
+++ client/e2e/vue.spec.ts
@@ -4,5 +4,5 @@ import { test, expect } from '@playwright/test';
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('p')).toHaveText('Relewise Candidate Assignment');
+  await expect(page.locator('footer a')).toHaveText('Relewise Candidate Assignment');
 })
